793bf542a0b501a1616f965369c44e169cfd563f
Vlad Kononov <vladimirkon@gmail.com>
2020-05-19 19:00:07 -0700

    First commit

---
+++ src/test/java/vladkononov/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,44 @@
+package vladkononov.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+	
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config() {
+		//ExtentReports, ExtentSparkReporter
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Vlad Kononov");
+	}
+	
+	@Test
+	public void initialDemo() {
+		ExtentTest test = extent.createTest("Initial Demo");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vladi\\workspace\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		//test.fail("Result don't match");
+		
+		
+		extent.flush();
+		
+	}
+
+}
